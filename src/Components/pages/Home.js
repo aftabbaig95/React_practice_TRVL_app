b17565a569707db44b92b46629675f8a0d535eff
Aftab Baig <aftab.a.baig@gmail.com>
2021-01-02 19:38:51 -0600

    first commit

---
+++ src/Components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Herosection from '../HeroSection';
+import Cards from '../Cards'; 
+import Footer from '../Footer';
+
+function Home(){
+    return(
+        <>
+            <Herosection/>
+            <Cards />
+            <Footer/>
+        </>
+    )
+}
+
+export default Home; 
